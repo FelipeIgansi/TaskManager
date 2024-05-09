47da194a67b87e013e586a1edaa745a706ccffa1
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-05-09 17:13:21 -0300

    Add new icon to save button

--- app/src/main/java/com/example/taskmananger/base/CallScaffold.kt
+++ app/src/main/java/com/example/taskmananger/base/CallScaffold.kt
@@ -1,10 +1,9 @@
 package com.example.taskmananger.base
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,8 +11,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.taskmananger.R
 import com.example.taskmananger.TaskAddViewModel
 import com.example.taskmananger.TaskEditViewModel
 import com.example.taskmananger.activity.TaskAdd
@@ -75,15 +78,20 @@ class CallScaffold(
     @Composable
     fun TaskEditTopBar() {
         CenterAlignedTopAppBar(
-            title = { Text(text = "Criar de nota") },
+            title = { Text(text = "Editar nota") },
             actions = {
                 IconButton(onClick = { taskEditViewModel.setIsSaveRequest(true) }) {
-                    Icon(Icons.Default.Done, contentDescription = null, tint = Color.Green)
+                    Icon(
+                        painterResource(id = R.drawable.save),
+                        contentDescription = null,
+                        tint = Color.Green,
+                        modifier = Modifier.size(25.dp)
+                    )
                 }
             },
             navigationIcon = {
-                IconButton(onClick = { taskEditViewModel.setIsSaveRequest(false) }) {
-                    Icon(Icons.Default.Close, contentDescription = null, tint = Color.Red)
+                IconButton(onClick = { navController.navigate(Routes.TaskList.route) }) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = null)
                 }
             }
         )
@@ -94,15 +102,20 @@ class CallScaffold(
     fun TaskAddTopBar() {
 
         CenterAlignedTopAppBar(
-            title = { Text(text = "Editar nota") },
+            title = { Text(text = "Criar de nota") },
             actions = {
                 IconButton(onClick = { taskAddViewModel.setIsSaveRequest(true) }) {
-                    Icon(Icons.Default.Done, contentDescription = null, tint = Color.Green)
+                    Icon(
+                        painterResource(id = R.drawable.save),
+                        contentDescription = null,
+                        tint = Color.Green,
+                        modifier = Modifier.size(25.dp)
+                    )
                 }
             },
             navigationIcon = {
-                IconButton(onClick = { taskAddViewModel.setIsSaveRequest(false) }) {
-                    Icon(Icons.Default.Close, contentDescription = null, tint = Color.Red)
+                IconButton(onClick = { navController.navigate(Routes.TaskList.route) }) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = null)
                 }
             }
         )
