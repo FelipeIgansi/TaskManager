1dd1b586d6fc2e48822a1740675d94706cceb407
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-05-14 22:33:49 -0300

    Change the Constants File

--- app/src/main/java/com/taskmanager/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.taskmanager
-
-class Constants {
-    companion object{
-        const val TITLE = "title"
-        const val CONTENT = "content"
-    }
-}
---
+++ app/src/main/java/com/taskmanager/base/Constants.kt
@@ -0,0 +1,20 @@
+package com.taskmanager.base
+
+class Constants {
+    companion object{
+        const val TITLE_KEY = "title"
+        const val CONTENT_KEY = "content"
+
+        const val TITLE = "Titulo"
+        const val DESCRIPTION = "Descricao"
+        const val YES = "Sim"
+        const val NO = "Nao"
+        const val CONFIRMAREXCLUSAO = "Tem certeza que deseja excluir?"
+        const val SEMREGISTROMSG = "Não há tarefas salvas"
+
+        const val EDITTASKTEXT = "Edição da nota"
+        const val DETAILTASKTEXT = "Detalhes da nota"
+        const val CREATETASKTEXT = "Criação de notas"
+        const val LISTTASKTEXT = "Minhas notas"
+    }
+}
