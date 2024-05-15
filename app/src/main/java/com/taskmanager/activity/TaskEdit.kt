7a11ac09fac7515b6a2211a0cef1a7f946a1d8a9
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-05-14 22:24:25 -0300

    Change the TaskEdit and TaskEditViewModel

--- app/src/main/java/com/taskmanager/activity/TaskEdit.kt
+++ app/src/main/java/com/taskmanager/activity/TaskEdit.kt
@@ -2,10 +2,7 @@ package com.taskmanager.activity
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -13,57 +10,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import com.taskmanager.Constants
 import com.taskmanager.activity.viewmodel.TaskEditViewModel
-import com.taskmanager.base.Routes
-import com.taskmanager.data.LocalTaskData
+import com.taskmanager.base.Constants
 
 @Composable
 fun TaskEdit(
     padding: PaddingValues,
-    navController: NavHostController,
-    localTaskData: LocalTaskData,
     taskEditViewModel: TaskEditViewModel
 ) {
-    var title by remember { mutableStateOf(localTaskData.get(Constants.TITLE)) }
-    var content by remember { mutableStateOf(localTaskData.get(Constants.CONTENT)) }
-    val isSaveRequested by taskEditViewModel.isSaveRequested.collectAsState()
+    val title by taskEditViewModel.title.collectAsState()
+    val description by taskEditViewModel.description.collectAsState()
+    val isSaveRequest by taskEditViewModel.isSaveRequest.collectAsState()
 
-    LaunchedEffect(isSaveRequested) {
-        if (isSaveRequested) {
-            localTaskData.save(Constants.TITLE, title)
-            localTaskData.save(Constants.CONTENT, content)
-            navController.navigate(Routes.TaskList.route)
-        }
-    }
+    LaunchedEffect(isSaveRequest) { if (isSaveRequest) taskEditViewModel.editarTask() }
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
             .padding(padding)
-            .padding(16.dp)
+            .padding(top = 20.dp, start = 10.dp, end = 10.dp)
     ) {
         OutlinedTextField(
             value = title,
-            onValueChange = { title = it },
-            label = { Text("Título") },
+            onValueChange = { taskEditViewModel.setTitle(it) },
+            label = { Text(text = Constants.TITLE) },
             modifier = Modifier.fillMaxWidth()
         )
-        Spacer(modifier = Modifier.height(8.dp))
         OutlinedTextField(
-            value = content,
-            onValueChange = { content = it },
-            label = { Text("Conteudo") },
+            value = description,
+            onValueChange = { taskEditViewModel.setDescription(it) },
+            label = { Text(Constants.DESCRIPTION) },
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f),
-            maxLines = Int.MAX_VALUE
+                .weight(1f)
         )
+
     }
 }
--- app/src/main/java/com/taskmanager/activity/viewmodel/TaskEditViewModel.kt
+++ app/src/main/java/com/taskmanager/activity/viewmodel/TaskEditViewModel.kt
@@ -1,14 +1,43 @@
 package com.taskmanager.activity.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.taskmanager.base.Constants
+import com.taskmanager.base.Routes
+import com.taskmanager.data.LocalTaskData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class TaskEditViewModel : ViewModel() {
-    private var _isSaveRequested = MutableStateFlow(false)
-    val isSaveRequested: StateFlow<Boolean> = _isSaveRequested
+class TaskEditViewModel(
+    private val navController: NavController,
+    private val localData: LocalTaskData
+) : ViewModel() {
 
-    fun setIsSaveRequest(value: Boolean) {
-        _isSaveRequested.value = value
+    private var _title = MutableStateFlow(localData.get(Constants.TITLE_KEY))
+    val title: StateFlow<String> = _title
+
+    private var _description = MutableStateFlow(localData.get(Constants.CONTENT_KEY))
+    val description: StateFlow<String> = _description
+
+    private var _isSaveRequest = MutableStateFlow(false)
+    val isSaveRequest: StateFlow<Boolean> = _isSaveRequest
+
+    fun setSaveRequest(value: Boolean) {
+        _isSaveRequest.value = value
+    }
+
+    fun setTitle(title: String) {
+        _title.value = title
+
+    }
+
+    fun setDescription(description: String) {
+        _description.value = description
+    }
+
+    fun editarTask() {
+        localData.save(Constants.TITLE_KEY, _title.value)
+        localData.save(Constants.CONTENT_KEY, _description.value)
+        navController.navigate(Routes.TaskList.route)
     }
 }
