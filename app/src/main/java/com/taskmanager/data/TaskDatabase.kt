18e50008423c213e3352e2c62c5192b6d4a0855a
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-06-04 15:15:40 -0300

    Create a TaskDatabase

--- app/src/main/java/com/taskmanager/data/TaskDatabase.kt
+++ app/src/main/java/com/taskmanager/data/TaskDatabase.kt
@@ -1,3 +1,33 @@
 package com.taskmanager.data
 
-class TaskDatabase {}
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [TaskEntity::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract  fun taskdao() : Taskdao
+
+    companion object{
+        @Volatile
+        private var INSTANCE : TaskDatabase? = null
+        fun getDatabase(context: Context): TaskDatabase{
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null){
+                return  tempInstance
+            }
+
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
--- app/src/main/java/com/taskmanager/data/Taskdao.kt
+++ app/src/main/java/com/taskmanager/data/Taskdao.kt
@@ -1,5 +1,4 @@
 package com.taskmanager.data
-
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
