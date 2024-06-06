62d5046d83f42f53bc57fa0a8e37841058be8768
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-06-06 15:15:59 -0300

    Change the way how tasks are saved, and  now used a object Tasks instead a String for title

--- app/src/main/java/com/taskmanager/activity/viewmodel/TaskListViewModel.kt
+++ app/src/main/java/com/taskmanager/activity/viewmodel/TaskListViewModel.kt
@@ -1,24 +1,36 @@
 package com.taskmanager.activity.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
-import com.taskmanager.base.Constants
-import com.taskmanager.data.LocalTaskData
+import com.taskmanager.data.TaskDatabase
+import com.taskmanager.data.TaskEntity
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class TaskListViewModel(private val localData: LocalTaskData) : ViewModel() {
+class TaskListViewModel(
+    private val localDB: TaskDatabase,
+) :
+    ViewModel() {
 
-    private var _title = MutableStateFlow(localData.get(Constants.TITLE_KEY))
-    val title: StateFlow<String> = _title
+    private var _tasks = MutableStateFlow<List<TaskEntity>>(emptyList())
+    val tasks : StateFlow<List<TaskEntity>> = _tasks
 
     private var _showAlertDialog = MutableStateFlow(false)
     val showAlertDialog: StateFlow<Boolean> = _showAlertDialog
 
+
+    fun loadTasks(){
+        viewModelScope.launch {
+            _tasks.value = localDB.taskdao().getAll()
+        }
+    }
+
     fun deleteTask() {
-        localData.delete(Constants.TITLE_KEY)
+        /*localData.delete(Constants.TITLE_KEY)
         localData.delete(Constants.CONTENT_KEY)
-        _title.value = ""
+        _title.value = ""*/
         _showAlertDialog.value = false
     }
 
