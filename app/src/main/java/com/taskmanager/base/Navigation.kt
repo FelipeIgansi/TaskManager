5c2f9be278eb67769f079e6559ce461cdca3b89c
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-06-06 14:19:37 -0300

    Add localDB in the navigation

--- app/src/main/java/com/taskmanager/base/Navigation.kt
+++ app/src/main/java/com/taskmanager/base/Navigation.kt
@@ -8,23 +8,26 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.taskmanager.data.LocalTaskData
+import com.taskmanager.data.TaskDatabase
 
 class Navigation {
     private lateinit var navController: NavHostController
     private lateinit var localTaskData: LocalTaskData
+    private lateinit var localdb: TaskDatabase
 
     private fun NavGraphBuilder.composableScreen(route: String) {
-        composable(route) { CallScaffold(navController, localTaskData).buildScreen(route) }
+        composable(route) { CallScaffold(navController, localTaskData, localdb).buildScreen(route) }
     }
 
     @Composable
     fun Create (){
         navController = rememberNavController()
         localTaskData = LocalTaskData(LocalContext.current)
+        localdb = TaskDatabase.getDatabase(LocalContext.current)
 
         val startDestination = Routes.TaskList.route
 
-        if (!startDestination.isNullOrEmpty()) {
+        if (startDestination.isNotEmpty()) {
             NavHost(
                 navController = navController,
                 startDestination = startDestination
