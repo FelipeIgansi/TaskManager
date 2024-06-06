da43c99d847fcb465a6ccb67406578502a532da9
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-06-06 14:58:44 -0300

    Change the method to add tasks to use the local database

--- app/src/main/java/com/taskmanager/activity/viewmodel/TaskAddViewModel.kt
+++ app/src/main/java/com/taskmanager/activity/viewmodel/TaskAddViewModel.kt
@@ -1,21 +1,29 @@
 package com.taskmanager.activity.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
-import com.taskmanager.base.Constants
 import com.taskmanager.base.Routes
 import com.taskmanager.data.LocalTaskData
+import com.taskmanager.data.TaskDatabase
+import com.taskmanager.data.TaskEntity
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class TaskAddViewModel (private val navController: NavController, private  val localData: LocalTaskData): ViewModel() {
+class TaskAddViewModel(
+    private val navController: NavController,
+    private val localData: LocalTaskData,
+    private val localDB: TaskDatabase
+) : ViewModel() {
+
     private var _title = MutableStateFlow("")
     val title: StateFlow<String?> = _title
 
-    private var _description = MutableStateFlow("")
-    val description: StateFlow<String?> = _description
+    private var _content = MutableStateFlow("")
+    val description: StateFlow<String?> = _content
 
-    private  var _isSaveRequest = MutableStateFlow(false)
+    private var _isSaveRequest = MutableStateFlow(false)
     val isSaveRequest: StateFlow<Boolean> = _isSaveRequest
 
     fun setSaveRequest(value: Boolean) {
@@ -23,8 +31,10 @@ class TaskAddViewModel (private val navController: NavController, private  val l
     }
 
     fun createTask() {
-        localData.save(Constants.TITLE_KEY, _title.value)
-        localData.save(Constants.CONTENT_KEY, _description.value)
+        viewModelScope.launch {
+            localDB.taskdao().insertAll(TaskEntity(0, _title.value, _content.value))
+        }
+
         navigate(Routes.TaskList.route)
     }
 
@@ -37,7 +47,6 @@ class TaskAddViewModel (private val navController: NavController, private  val l
     }
 
     fun setDescription(description: String) {
-        _description.value = description
+        _content.value = description
     }
-
 }
