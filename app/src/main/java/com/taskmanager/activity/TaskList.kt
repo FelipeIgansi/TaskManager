14af1125c72b5ef4821858c8762147af7bd3f405
ProfFelipeIgansi <proffelipeigansi@gmail.com>
2024-06-06 15:18:04 -0300

    Change the observable variable for an object task instead a String and add a list to show all tasks

--- app/src/main/java/com/taskmanager/activity/TaskList.kt
+++ app/src/main/java/com/taskmanager/activity/TaskList.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -20,14 +21,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.taskmanager.base.Constants
 import com.taskmanager.activity.viewmodel.TaskListViewModel
+import com.taskmanager.base.Constants
 import com.taskmanager.base.Routes
 
 @Composable
@@ -36,7 +38,10 @@ fun TaskList(
     navController: NavHostController,
     listViewModel: TaskListViewModel
 ) {
-    val title by listViewModel.title.collectAsState()
+
+    LaunchedEffect(key1 = Unit) { listViewModel.loadTasks() }
+//    val title by listViewModel.title.collectAsState()
+    val tasks by listViewModel.tasks.collectAsState()
     val showAlertDialog by listViewModel.showAlertDialog.collectAsState()
 
     Column(
@@ -61,47 +66,57 @@ fun TaskList(
                 text = { Text(text = Constants.CONFIRMAREXCLUSAO) }
             )
         }
-        if (title != "") {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-                    .clickable {
-                        listViewModel.navigate(
-                            Routes.TaskDetail.route,
-                            navController
-                        )
-                    }
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = title ?: "",
-                        modifier = Modifier.padding(
-                            start = 20.dp,
-                            end = 20.dp,
-                            top = 10.dp,
-                            bottom = 10.dp
-                        )
-                    )
-                    Box(modifier = Modifier) {
-                        Row {
-                            IconButton(onClick = { listViewModel.setShowAlertDialog(true) }) {
-                                Icon(Icons.Default.Delete, contentDescription = null)
-                            }
-                            IconButton(onClick = {
-                                listViewModel.navigate(Routes.TaskEdit.route, navController)
-                            }) {
-                                Icon(Icons.Default.Edit, contentDescription = null)
-                            }
-                        }
+        if (tasks.isNotEmpty()) {
+            LazyColumn {
+                tasks.forEach { task ->
+                    item {
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                                .clickable {
+                                    listViewModel.navigate(
+                                        Routes.TaskDetail.route,
+                                        navController
+                                    )
+                                }
+                        ) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(
+                                    text = task.title,
+                                    modifier = Modifier.padding(
+                                        start = 20.dp,
+                                        end = 20.dp,
+                                        top = 10.dp,
+                                        bottom = 10.dp
+                                    )
+                                )
+                                Box(modifier = Modifier) {
+                                    Row {
+                                        IconButton(onClick = { listViewModel.setShowAlertDialog(true) }) {
+                                            Icon(Icons.Default.Delete, contentDescription = null)
+                                        }
+                                        IconButton(onClick = {
+                                            listViewModel.navigate(
+                                                Routes.TaskEdit.route,
+                                                navController
+                                            )
+                                        }) {
+                                            Icon(Icons.Default.Edit, contentDescription = null)
+                                        }
+                                    }
 
+                                }
+                            }
+
+                        }
                     }
                 }
-
             }
+
         } else {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 Text(text = Constants.SEMREGISTROMSG)
