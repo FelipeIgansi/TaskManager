9d33701a9c98a7d958501283afe8b4dbd1815f69
FelipeIgansi <lipeigansi@gmail.com>
2024-07-15 16:24:21 -0300

    Create a screen and viewmodel to log in user.

---
+++ app/src/main/java/com/taskmanager/activity/LoginScreen.kt
@@ -0,0 +1,83 @@
+package com.taskmanager.activity
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.taskmanager.activity.viewmodel.LoginViewModel
+import com.taskmanager.base.Routes
+
+@Composable
+fun LoginScreen(viewModel: LoginViewModel) {
+    val email by viewModel.email.collectAsState()
+    val password by viewModel.pass.collectAsState()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp), contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Login",
+                fontSize = 32.sp,
+                style = TextStyle(fontWeight = FontWeight.Bold)
+            )
+            OutlinedTextField(
+                value = email, onValueChange = { viewModel.setEmail(it) },
+                label = { Text(text = "E-mail") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Email
+                )
+            )
+            OutlinedTextField(
+                value = password, onValueChange = { viewModel.setPassword(it) },
+                label = { Text(text = "Password") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                Column {
+                    Button(
+                        onClick = { /*TODO*/ }, modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    ) {
+                        Text(text = "Login")
+                    }
+                    OutlinedButton(
+                        onClick = { viewModel.navigate(Routes.CreateAccount.route) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                    ) {
+                        Text(text = "Register")
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/taskmanager/activity/viewmodel/LoginViewModel.kt
@@ -0,0 +1,26 @@
+package com.taskmanager.activity.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class LoginViewModel(private val navController: NavController) : ViewModel() {
+    private var _email = MutableStateFlow("")
+    val email: StateFlow<String> = _email
+
+    private var _pass = MutableStateFlow("")
+    val pass: StateFlow<String> = _pass
+
+    fun setEmail(value: String) {
+        _email.value = value
+    }
+
+    fun setPassword(value: String) {
+        _pass.value = value
+    }
+
+    fun navigate(destination: String) {
+        navController.navigate(destination)
+    }
+}
