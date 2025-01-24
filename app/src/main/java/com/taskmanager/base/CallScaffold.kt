fc8064d86de131c4d83a84b2b4707148adc8f78f
FelipeIgansi <lipeigansi@gmail.com>
2025-01-24 16:35:05 -0300

    Aula 02 - Refatoração: removendo o task detail

--- app/src/main/java/com/taskmanager/base/CallScaffold.kt
+++ app/src/main/java/com/taskmanager/base/CallScaffold.kt
@@ -24,7 +24,6 @@ import com.taskmanager.activity.CreateAccountScreen
 import com.taskmanager.activity.LoginScreen
 import com.taskmanager.activity.SyncDatabaseScreen
 import com.taskmanager.activity.TaskAdd
-import com.taskmanager.activity.TaskDetail
 import com.taskmanager.activity.TaskEdit
 import com.taskmanager.activity.TaskList
 import com.taskmanager.activity.WelcomeScreen
@@ -32,7 +31,6 @@ import com.taskmanager.activity.viewmodel.CreateAccountViewModel
 import com.taskmanager.activity.viewmodel.LoginViewModel
 import com.taskmanager.activity.viewmodel.SyncDatabaseViewModel
 import com.taskmanager.activity.viewmodel.TaskAddViewModel
-import com.taskmanager.activity.viewmodel.TaskDetailViewModel
 import com.taskmanager.activity.viewmodel.TaskEditViewModel
 import com.taskmanager.activity.viewmodel.TaskListViewModel
 import com.taskmanager.activity.viewmodel.WelcomeViewModel
@@ -54,7 +52,6 @@ class CallScaffold(
     private val taskAddViewModel by lazy { TaskAddViewModel(navController, localdb, cloudDB, auth) }
     private val taskEditViewModel by lazy { TaskEditViewModel(navController, localTaskData, localdb, cloudDB, auth) }
     private val taskListViewModel by lazy { TaskListViewModel(localdb, auth, cloudDB) }
-    private val taskdetailViewModel by lazy { TaskDetailViewModel(localTaskData, localdb) }
     private val createAccountViewModel by lazy { CreateAccountViewModel(navController, auth, sessionAuth, cloudDB) }
     private val loginViewModel by lazy { LoginViewModel(navController, auth, sessionAuth) }
     private val welcomeViewModel by lazy { WelcomeViewModel(navController, sessionAuth) }
@@ -63,7 +60,6 @@ class CallScaffold(
     @Composable
     fun buildScreen(screen: String): PaddingValues {
         val viewModel = when (screen) {
-            Routes.TaskDetail.route -> taskdetailViewModel
             Routes.TaskAdd.route -> taskAddViewModel
             Routes.TaskEdit.route -> taskEditViewModel
             Routes.TaskList.route -> taskListViewModel
@@ -75,7 +71,6 @@ class CallScaffold(
         }
         Scaffold(topBar = { CustomTopAppBar(screen = screen, viewModel = viewModel) }) { padding ->
             when (screen) {
-                Routes.TaskDetail.route -> TaskDetail(padding, taskdetailViewModel)
                 Routes.TaskAdd.route -> TaskAdd(padding, taskAddViewModel)
                 Routes.TaskEdit.route -> TaskEdit(padding, taskEditViewModel)
                 Routes.TaskList.route -> TaskList(padding, navController, taskListViewModel, localTaskData)
@@ -111,7 +106,6 @@ class CallScaffold(
                 when (viewModel) {
                     is TaskAddViewModel,
                     is TaskEditViewModel,
-                    is TaskDetailViewModel,
                     is CreateAccountViewModel,
                     is LoginViewModel -> {
                         IconButton(onClick = { navController.navigateUp() }) {
